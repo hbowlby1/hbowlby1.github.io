ea7117622136d619207150d2d16bfd8bae03cb23
hrbowlby <hrbowlby@gmail.com>
2021-03-13 20:32:24 -0700

    update

---
+++ lesson-10/js/getDate.js
@@ -0,0 +1,28 @@
+// setting the functions
+getTheDate();
+lastModifiedDoc();
+
+// gets the current date
+function getTheDate() {
+
+    
+    
+    let d = new Date();
+
+    let theYear = d.getFullYear().toString();
+
+    document.querySelector(".getDate").innerHTML = "&copyBeyond the Weather " + theYear;
+}
+
+//gets the date for when the document was last modified
+
+function lastModifiedDoc() {
+    let d = new Date();
+    var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "Ocotber", "November", "December"];
+    document.querySelector("#theDate").innerHTML = days[d.getDay()] + ", " + d.getDate() + " "+ month[d.getMonth()] + " " +  d.getFullYear();
+
+    if(days[d.getDay()] == "Friday"){
+        document.querySelector(".pancake").style.display = "block";
+    }
+}
