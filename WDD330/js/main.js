59efafdc49b31fe8407b904093c88c52c1b4895d
hbowlby1 <hrbowlby@gmail.com>
2021-09-23 18:41:06 -0700

    updatedd javascript for a tags on seperate lines

--- WDD330/js/main.js
+++ WDD330/js/main.js
@@ -14,30 +14,46 @@ const links = [
 
 const theList = document.querySelector('#wdd330Assign');
 
-
-// loops through the links array and grabs the label and sets it in the li
 for (i = 0; i <= links.length - 1; i++) {
-    const li = document.createElement('li');
+    const a = document.createElement('a');
+    const br = document.createElement('br');
     //adding the id addLink so I can loop through and add the a tags inside the li
-    li.setAttribute('id', 'addLink');
-    li.innerText = links[i].label + ': '
+    a.setAttribute('href', 'links[i].url');
+    a.innerText = links[i].label;
 
     // const a = document.createElement('a')
     // a.setAttribute('href', links[i].url);
 
-    theList.appendChild(li);
+    theList.appendChild(a);
+    theList.appendChild(br);
+
+// all code below is being reviewed and reworked.
+
+// loops through the links array and grabs the label and sets it in the li
+// for (i = 0; i <= links.length - 1; i++) {
+//     const li = document.createElement('li');
+//     //adding the id addLink so I can loop through and add the a tags inside the li
+//     li.setAttribute('id', 'addLink');
+//     li.innerText = links[i].label + ': '
+
+//     // const a = document.createElement('a')
+//     // a.setAttribute('href', links[i].url);
+
+//     theList.appendChild(li);
 }
 
 //grabs the li nested in the ul 
 const theLi = document.querySelector('#addLink');
 
+
+//code below needs to be re-worked until I can find a solution on putting the a tags on the same line as the li.
 // loops again to add the links to the li element
 
 
-for (i = 0; i <= links.length - 1; i++){
-    const a = document.createElement('a');
-    a.setAttribute('href', links[i].url);
-    a.innerText = "Click Here";
+// for (i = 0; i <= links.length - 1; i++){
+//     const a = document.createElement('a');
+//     a.setAttribute('href', links[i].url);
+//     a.innerText = "Click Here";
 
-    theLi.appendChild(a);
-}
+//     theLi.appendChild(a);
+// }
